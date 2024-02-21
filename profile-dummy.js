b2c62344a36f298511010775ffeb37a3366aab48
r-iradukunda <r.iradukund@alustudent.com>
2024-02-21 13:54:20 +0200

    Resume

---
+++ profile-dummy.js
@@ -0,0 +1,73 @@
+const profileData = {
+    title: 'Profile',
+    name: 'Ruth Iradukunda',
+    sub_title: 'Software Engineer',
+    about: {
+      intro: ` As a software engineering student, I'm dedicated to mastering the intricacies of designing, developing, and maintaining innovative software solutions. 
+      With a passion for problem-solving and a commitment to continuous learning, I aim to contribute to advancements in technology and make a meaningful impact 
+      in the ever-evolving field of software engineering.`,
+      contact: {
+        email: 'r.Iradukund@alustudent.com',
+        phone: '+250783445076',
+        address: 'Kigali , Rwanda'
+      }
+    },
+    links: [
+      { title: 'Google', src: 'https://www.google.com/' },
+      { title: 'Facebook', src: 'https://www.facebook.com/' }
+    ],
+    education: [
+      {
+        alma: "African Leadership University",
+        duration: "2023 - 2026",
+        std: "BSE. (software Engineering)",
+      },
+      {
+        alma: "FAWE Girl's School",
+        duration: "2017 - 2019",
+        std: "A level (MPC)",
+        score: "95.89%"
+      }
+    ],
+    skills: [
+      {
+        category: "Programming Languages",
+        topics: ["HTML", "CSS", "JavaScript", "Python"]
+      },
+      {
+        category: "Frameworks / Libraries",
+        topics: [
+          "NodeJS",
+          "Express",
+          "NextJS",
+          "Bootstrap",
+        ]
+      }
+    ],
+    projects: [
+      {
+        title: "Quiz App",
+        duration: "30 May 2023",
+        link: "https://github.com/r-iradukunda/quiz-app",
+        desc: `An English quiz application that asks questions and fill the gaps. each question takes forty seconds and Each question has its only one corresponding answer`
+      }
+    ],
+    experiences: [
+      {
+        organization: 'Guezshow',
+        title: `Animator`,
+        date: '2022 - 2023',
+        details: [
+          `Passionate animator with a flair for storytelling and bringing characters to life through captivating motion. Proficient in various animation techniques and software, adept at collaborating with teams to create visually stunning and emotionally resonant animations. Dedicated to pushing creative boundaries and delivering memorable experiences to audiences worldwide.`
+        ]
+      }
+    ],
+    
+    certifications: [
+      {
+        desc: `IEEE and its members inspire a global community to innovate for a better tomorrow through highly cited publications, conferences, technology standards, and professional and educational activities. IEEE is the trusted “voice” for engineering, computing, and technology information around the globe.`,
+        date: 'Nov 2022'
+      },
+    ],
+    events: []
+  };
---
+++ profile.js
@@ -0,0 +1,100 @@
+const profileData = {
+    title: "Resume",
+    name: "Ruth Iradukunda",
+    sub_title: "Software Engineer",
+    about: {
+      intro: `As a software engineering student, I'm dedicated to mastering the intricacies of designing, developing, and maintaining innovative software solutions. 
+      With a passion for problem-solving and a commitment to continuous learning, I aim to contribute to advancements in technology and make a meaningful impact 
+      in the ever-evolving field of software engineering.`,
+      contact: {
+        email: "r.iradukund@alustudent.com",
+        phone: "+250783445076",
+        address: "Kigali, Gasabo"
+      }
+    },
+    links: [
+      { title: "Github", src: "https://github.com/r-iradukunda" },
+      { title: "LinkedIn", src: "https://www.linkedin.com/in/iradukunda-ruth-748303192/" }
+    ],
+    education: [
+      {
+        alma: "African Leadership University",
+        duration: "2023 - 2026",
+        std: "BSE. (software Engineering)",
+      },
+      {
+        alma: "FAWE Girl's School",
+        duration: "2017 - 2019",
+        std: "A level (MPC)",
+        score: "95.89%"
+      },
+      {
+        alma: "ENDP (Ecole Notre Dame de la Providance)",
+        duration: "2014 - 2016",
+        std: "O level",
+        score: "70%"
+      },
+      {
+      alma: "Espoir",
+        duration: "2008 - 2013",
+        std: "Primary School",
+        score: "80.78%"
+      }
+    ],
+    skills: [
+      {
+        category: "Programming Languages",
+        topics: ["HTML", "CSS", "JavaScript", "Python"]
+      },
+      {
+        category: "Frameworks / Libraries",
+        topics: [
+          "NodeJS",
+          "Express",
+          "NextJS",
+          "Bootstrap",
+        ]
+      }
+    ],
+    projects: [
+      {
+        title: "Quiz App",
+        duration: "30 May 2023",
+        link: "https://github.com/r-iradukunda/quiz-app",
+        desc: `An English quiz application that asks questions and fill the gaps. each question takes forty seconds and Each question has its only one corresponding answer`
+      }
+    ],
+    experiences: [
+      {
+        organization: "Guezshow",
+        title: `Animator`,
+        date: "2022 - 2023",
+        details: [
+          `Passionate animator with a flair for storytelling and bringing characters to life through captivating motion. Proficient in various animation techniques and software, adept at collaborating with teams to create visually stunning and emotionally resonant animations. Dedicated to pushing creative boundaries and delivering memorable experiences to audiences worldwide.`
+        ]
+      },
+      {
+        organization: "ZIP Technology",
+        title: ` Software patching and updating and Hardware testing and maintenance`,
+        date: "2020 - 2021",
+        details: [
+          `Proficient in software patching and updating procedures, ensuring systems remain secure and up-to-date. Skilled in hardware testing and maintenance, adept at diagnosing and resolving issues to optimize performance and reliability. Continuously strive to enhance system efficiency through meticulous attention to detail and proactive problem-solving approaches.`
+        ]
+      }
+    ],
+    certifications: [
+      {
+        desc: `<strong>IEEE</strong> 
+        IEEE and its members inspire a global community to innovate for a better tomorrow through highly cited publications, conferences, technology standards, and professional and educational activities. IEEE is the trusted “voice” for engineering, computing, and technology information around the globe.  (<a target='_blank' rel='noreferrer' href='https://neveragainrwanda.org/'>
+        https://drive.google.com/drive/u/0/home</a>)`,
+        date: "Nov 2022"
+      },
+      {
+        desc: `<strong>Never Again Rwanda</strong> 
+        We are a Rwandan peacebuilding and social justice non-governmental organization that was established in 2002, in response to the 1994 Genocide against the Tutsi.We aim to empower citizens of diverse backgrounds from Rwanda and the Great Lakes region with opportunities to become active citizens for a peaceful society.  (<a target='_blank' rel='noreferrer' href='https://neveragainrwanda.org/'>
+        https://drive.google.com/drive/u/0/home/</a>)`,
+        date: "Aug 2019"
+      }
+    ],
+    events: []
+  };
